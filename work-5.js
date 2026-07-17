615d33cfdace40bd463361363a880c59490c1c14
Andrey Isenko <aisenko87@gmail.com>
2026-07-17 23:24:57 +0300

    add time

--- work-5.js
+++ work-5.js
@@ -3,7 +3,7 @@
 fetch('./exempl.json')
   .then(resp => resp.json())
   .then(data => {
-    console.log(data);
+    // console.log(data);
     // console.log(data.name);
     // console.log(data.name || data.age);
     // console.log(data.name && data.age);
@@ -25,7 +25,36 @@ const obj = {
   age: 55,
   isActive: true,
 };
-console.log(obj);
+const ones = {
+  userName: 'Coco',
+  age: 51,
+  isActive: false,
+};
+// console.log  (obj);
 const one = JSON.stringify(obj);
-console.log(one);
+// console.log(one);
 localStorage.setItem('new', one);
+// console.log(JSON.stringify(32));
+// console.log(one);
+const parsed = JSON.parse(one);
+const onesStr = JSON.stringify(ones);
+// console.log(parsed);
+try {
+  const newObj = JSON.parse(onesStr);
+  //   console.log(newObj);
+} catch (err) {
+  console.log(err.stack);
+}
+// console.log('after error:', ones);
+// console.log(1781027752615);
+// const data = new Date(1781027752615);
+// console.log(data);
+// console.log(data.getDay());
+// console.log(data.getDate());
+// console.log(data.getMinutes());
+const timestamp = 1781027752615;
+
+const date = new Date(timestamp);
+
+console.log(date.toLocaleDateString());
+// Наприклад: 09.06.2026
